7f09025e8ae8fc8e961322ac1b61b45ddb893b27
Taro Fukunaga <tarof429@gmail.com>
2020-08-03 17:09:24 -0700

    Get round robin working

--- go.mod
+++ go.mod
@@ -3,10 +3,21 @@ module github.com/tarof429/nginx-mini-cluster
 go 1.14
 
 require (
+	docker.io/go-docker v1.0.0 // indirect
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.1
 	github.com/tarof429/nmc v0.0.0
+	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
+	golang.org/x/sys v0.0.0-20200803150936-fd5f0c170ac3 // indirect
 )
 
 replace github.com/tarof429/nmc => ./nmc
--- nmc/Server.go
+++ nmc/Server.go
@@ -1,12 +1,29 @@
 package nmc
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/mount"
+	"github.com/docker/docker/client"
+	"github.com/docker/go-connections/nat"
 )
 
+// internal counter used to track which port to forward requests to
 var forwardPortCounter int
+
+// List of forwarding ports
 var forwardPorts = []string{"3001", "3002"}
+
+//var forwardPorts = []string{"3001"}
+
+const imageName = "docker.io/library/nginx:latest"
+const containerName = "nginx"
 
 func GetRoundRobinForwardPort() string {
 
@@ -27,12 +44,74 @@ func RoundRobinHandler(w http.ResponseWriter, r *http.Request) {
 	forwardPort := GetRoundRobinForwardPort()
 
 	switch method := r.Method; method {
+
 	case "GET":
+
 		fmt.Println("Forwarding request to port: " + forwardPort)
+
+		http.Redirect(w, r, "http://localhost:"+forwardPort, http.StatusOK)
+
 	}
 }
 
+func StartNginx() {
+	fmt.Println("Pulling latest nginx image...")
+
+	ctx := context.Background()
+	cli, err := client.NewEnvClient()
+
+	if err != nil {
+		panic(err)
+	}
+
+	var options types.ImagePullOptions
+
+	reader, err := cli.ImagePull(ctx, imageName, options)
+
+	if err != nil {
+		panic(err)
+	}
+	io.Copy(os.Stdout, reader)
+
+	for i := 0; i < len(forwardPorts); i++ {
+
+		// Portable container configuration
+		var config = container.Config{Image: imageName}
+
+		// Non-portable container configuraton
+		var portMap = make(nat.PortMap)
+		port, _ := nat.NewPort("tcp", "80")
+		var pb nat.PortBinding
+		pb.HostIP = "0.0.0.0"
+		pb.HostPort = forwardPorts[i]
+
+		portMap[port] = []nat.PortBinding{pb}
+
+		mounts := []mount.Mount{}
+		m := mount.Mount{Source: "/home/taro/Code/Go/nginx-mini-cluster/site", Target: "/usr/share/nginx/html", Type: mount.TypeBind}
+		mounts = append(mounts, m)
+
+		var hostConfig = container.HostConfig{AutoRemove: true, PortBindings: portMap, Mounts: mounts}
+
+		resp, err := cli.ContainerCreate(ctx, &config, &hostConfig, nil, containerName+"-"+forwardPorts[i])
+
+		if err != nil {
+			panic(err)
+		}
+
+		if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+			panic(err)
+		}
+
+		fmt.Println(resp.ID)
+	}
+
+	defer reader.Close()
+}
+
 func Run() {
+
+	StartNginx()
 
 	http.HandleFunc("/", RoundRobinHandler)
 	fmt.Println("Server starting...")
